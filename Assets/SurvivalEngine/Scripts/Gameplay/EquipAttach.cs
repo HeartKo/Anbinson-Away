8845163cd23b9f0b216488e5f8458c385a430006
HeartKo <69335537+HeartKo@users.noreply.github.com>
2020-10-21 15:03:06 +0900

    First

---
+++ Assets/SurvivalEngine/Scripts/Gameplay/EquipAttach.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A location on a character to attach equipment (like hand, head, feet, ...)
+/// </summary>
+
+public class EquipAttach : MonoBehaviour
+{
+
+    //장비를 붙이다
+    public EquipSlot slot;
+    public EquipSide side;
+
+    public Transform[] childs;
+
+}
